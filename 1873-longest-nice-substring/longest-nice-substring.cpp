eb1b429f02e16a11204a2d0b8ff573996bc9dcf7
Dhyey Marvaniya <dhyeykumar.marvaniya127647@marwadiuniversity.ac.in>
2026-08-04 19:47:28 +0530

    Time: 10 ms (35.26%) | Memory: 14.1 MB (61.80%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.cpp
+++ 1873-longest-nice-substring/longest-nice-substring.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     string longestNiceSubstring(string s) {
-        if (s.length() < 2)
+        if (s.size() < 2)
             return "";
 
         unordered_set<char> st(s.begin(), s.end());
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.size(); i++) {
             if (st.count(tolower(s[i])) && st.count(toupper(s[i])))
                 continue;
 
             string left = longestNiceSubstring(s.substr(0, i));
             string right = longestNiceSubstring(s.substr(i + 1));
 
-            return (left.length() >= right.length()) ? left : right;
+            return left.size() >= right.size() ? left : right;
         }
 
         return s;
