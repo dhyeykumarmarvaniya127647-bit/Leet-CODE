7b05f1bbff22119e1d362c795dbe3b4a5d9e15b1
Dhyey Marvaniya <dhyeykumar.marvaniya127647@marwadiuniversity.ac.in>
2026-08-07 19:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (88.32%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -9,7 +9,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0)
-                nums[i] = temp[mid--];
+             nums[i] = temp[mid--];
             else
                 nums[i] = temp[high--];
         }
